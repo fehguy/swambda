1c775da8be5cd3a46212b7e48f94de4cf0cc40ae
Tony Tam <tony@eatbacon.org>
2018-05-04 12:28:32 -0700

    more tests

--- src/params.js
+++ src/params.js
@@ -5,22 +5,83 @@ module.exports.extract = (param, source) => {
     return;
   }
   try {
-    switch(param.type) {
-      case "integer":
-        const result = parseInt(value);
-        return isNaN(result) ? undefined : result;
-      case "boolean":
-        return value === "true" || value === true;
-      case "string":
-        if(param.format === "date-time") {
-          return new Date(value);
-        }
-        return value;
-      default:
-        return value;
+    if(param.collectionFormat) {
+      let parts, output = [];
+      switch(param.collectionFormat) {
+        case "csv":
+          parts = value.split(",");
+          parts.forEach((part) => {
+            const val = extractOne(param, part.trim());
+            if(typeof val !== "undefined") {
+              output.push(val);
+            }
+          });
+          return output;
+        case "tsv":
+          parts = value.split("\t");
+          parts.forEach((part) => {
+            const val = extractOne(param, part.trim());
+            if(typeof val !== "undefined") {
+              output.push(val);
+            }
+          });
+          return output;
+        case "ssv":
+          parts = value.split(" ");
+          parts.forEach((part) => {
+            const val = extractOne(param, part.trim());
+            if(typeof val !== "undefined") {
+              output.push(val);
+            }
+          });
+          return output;
+        default:
+          if(Array.isArray(value)) {
+            value.forEach((v) => {
+              const val = extractOne(param, part.trim());
+              if(typeof val !== "undefined") {
+                output.push(val);
+              }
+            })
+          }
+          return output;
+      }
+    }
+    else {
+      return extractOne(param, value);
     }
   }
   catch (e) {
 
   }
 };
+
+const extractOne = (param, value) => {
+  switch(param.type) {
+    case "integer":
+      const result = parseInt(value);
+      return isNaN(result) ? undefined : result;
+      break;
+    case "boolean":
+      if(value === "true" || value === true) {
+        return true;
+      }
+      if(value === "false" || value === false) {
+        return false;
+      }
+      return;
+    case "string":
+      if(param.format === "date-time") {
+        const date = new Date(value);
+        if(isNaN(date.getDate())) {
+          return;
+        }
+        else {
+          return date;
+        }
+      }
+      return value;
+    default:
+      return value;
+  }
+}
--- test/parameterExtraction.js
+++ test/parameterExtraction.js
@@ -2,7 +2,7 @@ const expect = require("expect");
 const params = require("../src/params");
 
 describe("parameter extraction", () => {
-  it("extracts an integer query parameters", () => {
+  it("extracts an integer query parameter", () => {
     const param = {
       in: "query",
       name: "id",
@@ -13,6 +13,33 @@ describe("parameter extraction", () => {
     const id = params.extract(param, {id: "100"});
     expect(id).toBe(100);
     expect(typeof id).toBe("number");
+  });
+
+  it("extracts an integer array parameters", () => {
+    const param = {
+      in: "query",
+      name: "ids",
+      type: "integer",
+      format: "int64",
+      collectionFormat: "csv",
+      required: true
+    };
+    const ids = params.extract(param, {ids: "100,101,102"});
+    expect(Array.isArray(ids)).toBe(true);
+    expect(ids.length).toBe(3);
+  });
+
+
+  it("ignores a mal-formed integer query param", () => {
+    const param = {
+      in: "query",
+      name: "id",
+      type: "integer",
+      format: "int64",
+      required: true
+    };
+    const id = params.extract(param, {id: "bad!"});
+    expect(id).not.toBeDefined();
   });
 
   it("extracts a boolean query parameter", () => {
@@ -27,6 +54,30 @@ describe("parameter extraction", () => {
     expect(typeof happy).toBe("boolean");
   });
 
+  it("extracts a boolean array query parameter", () => {
+    const param = {
+      in: "query",
+      name: "moods",
+      type: "boolean",
+      collectionFormat: "ssv",
+      required: true
+    };
+    const moods = params.extract(param, {moods: "true false true true"});
+    expect(Array.isArray(moods)).toBe(true);
+    expect(moods.length).toBe(4);
+  });
+
+  it("ignores a mal-formed boolean query param", () => {
+    const param = {
+      in: "query",
+      name: "happy",
+      type: "boolean",
+      required: true
+    };
+    const id = params.extract(param, {happy: "maybe?"});
+    expect(id).not.toBeDefined();
+  });
+
   it("extracts a date query parameter", () => {
     const param = {
       in: "query",
@@ -37,6 +88,94 @@ describe("parameter extraction", () => {
     };
     const birthday = params.extract(param, {birthday: "2015-10-26T07:46:36.611Z"});
     expect(birthday.toString()).toBe(new Date("2015-10-26T07:46:36.611Z").toString());
-    expect(typeof birthday).toBe("object");
   });
+
+  it("extracts a date array query parameter", () => {
+    const param = {
+      in: "query",
+      name: "dates",
+      type: "string",
+      format: "date-time",
+      collectionFormat: "tsv",
+      required: true
+    };
+    const dates = params.extract(param, {dates: "2015-10-26T07:46:36.611Z\t2016-10-26T07:46:36.611Z\t2017-10-26T07:46:36.611Z\t"});
+    expect(Array.isArray(dates)).toBe(true);
+    expect(dates.length).toBe(3);
+  });
+
+  it("ignores a mal-formed date query param", () => {
+    const param = {
+      in: "query",
+      name: "ipoDate",
+      type: "string",
+      format: "date-time",
+      required: true
+    };
+    const ipoDate = params.extract(param, {ipoDate: "hmmm"});
+    expect(ipoDate).not.toBeDefined();
+  });
+
+  it("extracts a string with whitespace", () => {
+    const param = {
+      in: "query",
+      name: "fullName",
+      type: "string",
+      required: true
+    };
+    const fullName = params.extract(param, {fullName: "Jebediah Springfield"});
+    expect(fullName).toBe("Jebediah Springfield");
+  });
+
+  it("extracts a csv string array", () => {
+    const param = {
+      in: "query",
+      name: "people",
+      type: "string",
+      collectionFormat: "csv",
+      required: true
+    };
+    const people = params.extract(param, {people: "Jesus,Lucifer"});
+    expect(Array.isArray(people)).toBe(true);
+    expect(people.length).toBe(2);
+  });
+
+  it("extracts a tsv string array", () => {
+    const param = {
+      in: "query",
+      name: "people",
+      type: "string",
+      collectionFormat: "tsv",
+      required: true
+    };
+    const people = params.extract(param, {people: "Bart\tLisa"});
+    expect(Array.isArray(people)).toBe(true);
+    expect(people.length).toBe(2);
+  });
+
+  it("extracts a ssv string array", () => {
+    const param = {
+      in: "query",
+      name: "people",
+      type: "string",
+      collectionFormat: "ssv",
+      required: true
+    };
+    const people = params.extract(param, {people: "Homer Marge"});
+    expect(Array.isArray(people)).toBe(true);
+    expect(people.length).toBe(2);
+  });
+
+  it("extracts a default string array", () => {
+    const param = {
+      in: "query",
+      name: "people",
+      type: "string",
+      required: true
+    };
+    const people = params.extract(param, {people: ["Homer", "Marge"]});
+    expect(Array.isArray(people)).toBe(true);
+    expect(people.length).toBe(2);
+  });
+
 });
