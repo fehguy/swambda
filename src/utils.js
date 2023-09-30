94a27705163cfd6bb0842451f807542fa3329e5e
Tony Tam <tony@eatbacon.org>
2023-09-30 10:27:31 -0700

    formatting

--- src/utils.js
+++ src/utils.js
@@ -3,48 +3,48 @@
 let container = {};
 
 module.exports.cacheWith = (cache) => {
-  const existing = container;
-  container = cache;
+    const existing = container;
+    container = cache;
 
-  if(existing.responseInterceptor) {
-    container.responseInterceptor = existing.responseInterceptor;
-  }
-  if(existing.extraHeaders) {
-    container.extraHeaders = existing.extraHeaders;
-  }
+    if (existing.responseInterceptor) {
+        container.responseInterceptor = existing.responseInterceptor;
+    }
+    if (existing.extraHeaders) {
+        container.extraHeaders = existing.extraHeaders;
+    }
 };
 
 module.exports.respondWith = function (resolve, code, body) {
-  const response = {
-    statusCode: code,
-    headers: {}
-  };
-  if(container.extraHeaders) {
-    Object.keys(container.extraHeaders).forEach((key) => {
-      response.headers[key] = container.extraHeaders[key];
-    });
-  }
+    const response = {
+        statusCode: code,
+        headers: {}
+    };
+    if (container.extraHeaders) {
+        Object.keys(container.extraHeaders).forEach((key) => {
+            response.headers[key] = container.extraHeaders[key];
+        });
+    }
 
-  if(typeof body !== "undefined") {
-    if(typeof body === "object") {
-      response.body = JSON.stringify(body, null, 2);
-      response.headers["Content-Type"] = "application/json";
+    if (typeof body !== "undefined") {
+        if (typeof body === "object") {
+            response.body = JSON.stringify(body, null, 2);
+            response.headers["Content-Type"] = "application/json";
+        }
+        else {
+            response.body = body;
+        }
     }
     else {
-      response.body = body;
+        response.body = "";
     }
-  }
-  else {
-    response.body = "";
-  }
-  if(container.responseInterceptor && typeof container.responseInterceptor === "function") {
-    container.responseInterceptor(response)
-      .then((res) => {
-        resolve(res);
-      }
-    );
-  }
-  else {
-    resolve(response);
-  }
+    if (container.responseInterceptor && typeof container.responseInterceptor === "function") {
+        container.responseInterceptor(response)
+            .then((res) => {
+                resolve(res);
+            }
+            );
+    }
+    else {
+        resolve(response);
+    }
 };
